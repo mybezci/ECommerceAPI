7d5dbb7055fe8e81afbfd7dd7038e1d7eda142c0
mybezci <mybezci@gmail.com>
2023-07-14 23:43:24 +0200

    inital repository design pattern

---
+++ Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+
+        //The data whose condition expression given to the function is true are queried and retrieved.
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
+
+        // The first unique data suitable for the condition expression given to the function is queried and retrieved.
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
+        Task<T> GetByIdAsync(string id);
+
+    }
+}
